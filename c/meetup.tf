66044885d88cb51bdfb779a012041b4891e393d3
gulyasm <mgulyas86@gmail.com>
2015-03-03 22:53:40 +0100

    Init commit.

---
+++ c/meetup.tf
@@ -0,0 +1,61 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "cloudmeetup-prod-webserver" {
+    ami = "ami-e4356d8c"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    security_groups = ["cloudmeetup-website"]
+    tags = {
+        Env = "prod"
+        Role = "webserver"
+        Project = "cloudmeetup"
+        Name = "cloudmeetup-prod-webserver"
+    }
+    key_name = "gulyasm_gulyasm"
+}
+resource "aws_instance" "cloudmeetup-prod-webserver2" {
+    ami = "ami-e4356d8c"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    security_groups = ["cloudmeetup-website"]
+    tags = {
+        Env = "prod"
+        Role = "webserver"
+        Project = "cloudmeetup"
+        Name = "cloudmeetup-prod-webserver"
+    }
+    key_name = "gulyasm_gulyasm"
+}
+
+resource "aws_elb" "cloudmeetup-prod-elb" {
+    name = "cloudmeetup-prod-elb"
+    availability_zones = ["us-east-1a"]
+    security_groups = ["sg-811eb2e4", "sg-f42cae90"]
+    listener = {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        target = "HTTP:80/index.html"
+        interval = 30
+    }
+    instances = ["${aws_instance.cloudmeetup-prod-webserver.id}", "${aws_instance.cloudmeetup-prod-webserver2.id}"]
+    cross_zone_load_balancing = true
+}
+
+
+output "ip" {
+    value = "${aws_elb.cloudmeetup-prod-elb.dns_name}"
+}
